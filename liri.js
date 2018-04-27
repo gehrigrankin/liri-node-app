a98b83757d6e889dc1f5e52e1a72da42fdb13913
gehrig rankin <rgrankin22@gmail.com>
2018-04-27 13:33:58 -0700

    added the movie-this feature

--- liri.js
+++ liri.js
@@ -4,11 +4,14 @@ var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var keys = require('./keys');
+var fs = require("fs");
+
  
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
  
 var command = process.argv[2];
+
 
 if (command == 'my-tweets'){
   var params = {screen_name: 'CodeGico'};
@@ -48,6 +51,41 @@ else if(command == 'spotify-this-song'){
 
 }
 else if(command == 'movie-this') {
-  
+  var nodeArgs = process.argv.splice(3);
 
+  var movieName = "";
+
+  for (var i = 0; i < nodeArgs.length; i++) {
+
+    if (i > 0 && i < nodeArgs.length) {
+      movieName = movieName + "+" + nodeArgs[i];
+    }
+    else {
+      movieName += nodeArgs[i];
+    }
+  }
+
+  var movieQuery = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=trilogy";
+
+
+  request(movieQuery, function(error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+      // console.log(response);
+  
+      // Parse the body of the site and recover just the imdbRating
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      console.log(
+        "\nTitle: " + JSON.parse(body).Title +
+        "\nRelease Year: " + JSON.parse(body).Year +
+        "\nIMDB Rating: " + JSON.parse(body).imdbRating +
+        "\nRotten Tomatoes Rating: " + JSON.parse(body).tomatoMeter +
+        "\nCountry of Origin: " + JSON.parse(body).Country +
+        "\nLanguage: " + JSON.parse(body).Language +
+        "\nPlot: " + JSON.parse(body).Plot +
+        "\nActors: " + JSON.parse(body).Actors
+      );
+    }
+  });
 }
